9e9c1dc3023f86e9dde15f811054a09badf15a25
afiq <afiq@nexea.co>
2025-06-17 14:57:57 +0800

    a

--- src/app/api/sheet/route.ts
+++ src/app/api/sheet/route.ts
@@ -7,8 +7,8 @@ const SCOPES = ['https://www.googleapis.com/auth/spreadsheets'];
 const keyPath = path.join(process.cwd(), 'eba-credentials.json');
 
 const auth = new google.auth.GoogleAuth({
-  credentials: JSON.parse(process.env.EBA_CREDENTIALS!),
-  // keyFile: JSON.parse(process.env.EBA_CREDENTIALS!) || keyPath,
+  // credentials: JSON.parse(process.env.EBA_CREDENTIALS!),
+  keyFile: '/secrets/eba-credentials',
   scopes: SCOPES,
 });
 
