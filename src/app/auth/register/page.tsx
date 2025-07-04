ff7e7f2f5926338797784676e1e26125da6db1e2
syafinion <syafiqmajid286@gmail.com>
2025-07-04 15:22:44 +0800

    adjust position of register form

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -6,17 +6,15 @@ import { RegisterForm } from '@/components/ui';
 
 const RegisterPage = () => {
   return (
-    <div className='min-h-svh w-full flex flex-col items-center justify-center p-4 md:p-8'>
-      <div className='w-full max-w-lg mx-auto text-center mb-8'>
-        <div className='flex items-center justify-center gap-4 mb-2'>
-          <Image alt='NEXEA Logo' height={40} src='/nexealogo.png' width={40} />
-          <h1 className='text-2xl sm:text-3xl font-bold tracking-tight'>
-            Entrepreneurs Behaviour Assessment
-          </h1>
-        </div>
+    <div className='flex min-h-svh w-full items-center justify-center p-6 md:p-10'>
+      <div className='flex items-center gap-4 absolute top-20 md:top-12 w-full p-6 justify-center'>
+        <Image alt='NEXEA Logo' height={40} src='/nexealogo.png' width={40} />
+        <h1 className='text-2xl sm:text-3xl font-bold tracking-tight'>
+          Entrepreneurs Behaviour Assessment
+        </h1>
       </div>
 
-      <div className='w-full max-w-sm mx-auto'>
+      <div className='w-full max-w-sm'>
         <RegisterForm />
       </div>
     </div>
