af7a7bb54d9d36774b27a49512cc9852a8490c64
afiq <afiq@nexea.co>
2025-09-12 15:21:09 +0800

    a

--- src/app/api/admin/allowlist/route.ts
+++ src/app/api/admin/allowlist/route.ts
@@ -112,7 +112,7 @@ export async function POST(req: NextRequest) {
       data: {
         addedBy: user?.email,
         credits,
-        email,
+        email: email.toLowerCase(),
       },
     });
 
